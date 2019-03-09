3aa937983ba692c501e60b6d68b5ef532e3e116e
ma-kun92 <c1nakata@yahoo.co.jp>
2019-03-09 21:38:18 +0900

    sign_up function for user
    メールアドレスを使用した場合のサインイン機能を実装しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :check, if: :devise_controller?
+  # before_action :sign_in_required
 private
   def after_sign_in_path_for(resource)
     root_path
@@ -17,9 +17,4 @@ private
   def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
   end
-
-  # def check
-  #   binding.pry
-  # end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
-  # devise_for :users, skip: :all
   devise_scope :user  do
     post 'registration' => 'devise/registrations#create', as: :create_registration
   end
 
   resources :users, only: [:index] do
     collection do
-      post 'register_user_tel'
       get 'register_user_address'
       post 'register_user_card'
       post 'register_user_complete'
       get 'logout'
     end
   end
+
   resources :items
   get 'items/:id/buy' => 'items#buy'
   root 'items#index'
+
 end
