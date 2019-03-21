3c9fd571a85f8afe5a9521583e6abcf726ae2109
ma-kun92 <c1nakata@yahoo.co.jp>
2019-03-21 20:31:11 +0900

    Edit carrierwave.rb
    edit carrrerwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,19 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#     region: 'ap-northeast-1'
+#   }
+
+#   config.fog_directory  = 'pictures-forest'
+#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pictures-forest'
+# end
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
@@ -9,17 +22,15 @@ CarrierWave.configure do |config|
     config.fog_directory  = '設定したバケット名'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
       path_style: true
     }
+    config.fog_directory  = 'pictures-forest'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pictures-forest'
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
-end
-
-  config.fog_directory  = 'pictures-forest'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pictures-forest'
 end
