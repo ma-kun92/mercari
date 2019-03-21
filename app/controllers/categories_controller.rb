6f91885837d849479183fbf51fa46a1afcfe6528
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-21 19:46:22 +0900

    modify_categories_show
    未ログイン時のアイテム情報追加
    出品者本人のアイテム閲覧中止

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,11 @@ class CategoriesController < ApplicationController
     @child_categories = Category.children_of(@category).order("RAND()").limit(9)
     progeny_category = Category.subtree_of(@category)
     sold_item = Deal.pluck('item_id')
-    @items = Item.includes(:category).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).order(id: :DESC)
+    if user_signed_in?
+      my_vendor = Vendor.find_by(user_id:current_user.id)
+      @items = Item.includes(:category,:vendor).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).where.not(vendor_id:my_vendor.id).order(id: :DESC)
+    else
+      @items = Item.includes(:category).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).order(id: :DESC)
+    end
   end
 end
