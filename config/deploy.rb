5ecf3678d82aa78a86e6de7c56e99643c6d199ef
ma-kun92 <c1nakata@yahoo.co.jp>
2019-03-22 19:32:35 +0900

    return changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :default_env, {
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
-set :linked_files, %w{ config/secrets.yml }
+set :linked_files, %w{ config/secrets.yml, .env }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
