aa6e5673bd48ed514bfce4dbead89f266409399d
ma-kun92 <c1nakata@yahoo.co.jp>
2019-03-22 12:27:01 +0900

    edit controller

--- app/controllers/users/cards_controller.rb
+++ app/controllers/users/cards_controller.rb
@@ -8,4 +8,16 @@ class Users::CardsController < ApplicationController
   add_breadcrumb '支払い方法', :card_path, only: [:show, :new]
   add_breadcrumb 'クレジットカード情報入力', :new_card_path, only: [:new]
 
+  private
+  def prepare_payjp
+    gon.payjp_pk_key =Rails.application.secrets.pay_pk_test
+    Payjp.api_key =Rails.application.secrets.pay_sk_test
+    unless current_user.payjp_id
+      current_user.payjp_id = Payjp::Customer.create(description: 'test').id
+      current_user.save
+    end
+    @customer = Payjp::Customer.retrieve(current_user.payjp_id)
+    @card = @customer.cards.retrieve(@customer.default_card) if @customer.cards.count > 0
+  end
+
 end
