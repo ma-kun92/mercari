bf11a9073e9f431158c9db6c903e5711f13c7395
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-23 19:36:21 +0900

    add_breadcrumb_function
    パンくず機能を編集

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :header_menu,only: [:index,:show]
+  add_breadcrumb 'メルカリ', :root_path
+  add_breadcrumb 'カテゴリー一覧', :categories_path
 
   def index
     @l_categories = Category.roots
@@ -11,5 +13,9 @@ class CategoriesController < ApplicationController
     progeny_category = Category.subtree_of(@category)
     sold_item = Deal.pluck('item_id')
     @items = Item.includes(:category).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).order(id: :DESC)
+    @category.ancestors.each do |category|
+      add_breadcrumb category.name, :categories_path
+    end
+    add_breadcrumb @category.name, :category_path
   end
 end
