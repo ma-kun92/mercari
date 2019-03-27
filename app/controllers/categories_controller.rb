bd7434e58de5bd193822e6d3fd286e71c0573b7c
katoShiro46 <work_h_t_m@yahoo.co.jp>
2019-03-27 15:25:45 +0900

    Edit_category_search
    サーチ画面を編集・リファクタリング実施

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,23 +4,25 @@ class CategoriesController < ApplicationController
   add_breadcrumb 'カテゴリー一覧', :categories_path
 
   def index
-    @l_categories = Category.roots
   end
 
   def show
     @category = Category.find(params[:id])
     @child_categories = Category.children_of(@category).order("RAND()").limit(9)
+    #一覧に表示するアイテムを取得（ログイン時、本人出品物は表示しない）
     progeny_category = Category.subtree_of(@category)
     sold_item = Deal.pluck('item_id')
-    if user_signed_in?
+    indicate_items = Item.includes(:category).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).order(id: :DESC)
+    if user_signed_in? && Vendor.find_by(user_id:current_user.id)
       my_vendor = Vendor.find_by(user_id:current_user.id)
-      @items = Item.includes(:category,:vendor).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).where.not(vendor_id:my_vendor.id).order(id: :DESC)
+      @items = indicate_items.where.not(vendor_id:my_vendor.id)
     else
-      @items = Item.includes(:category).where(categories:{id:progeny_category.ids}).where.not(id:sold_item).order(id: :DESC)
+      @items = indicate_items
     end
+    #パンくずリストの表示
     @category.ancestors.each do |category|
-      add_breadcrumb category.name, :categories_path
+      add_breadcrumb category.name, "#{category.id}"
     end
-    add_breadcrumb @category.name, :category_path
+    add_breadcrumb @category.name
   end
 end
